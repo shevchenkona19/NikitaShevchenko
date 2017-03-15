67bcf46ae379d9bdb015c7dd2f0bfad22b52b8cf
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-03-15 17:32:18 +0200

    Drawer now works

--- app/src/main/java/sheva/cardapptrue/MainActivity.java
+++ app/src/main/java/sheva/cardapptrue/MainActivity.java
@@ -1,5 +1,7 @@
 package sheva.cardapptrue;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -11,8 +13,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.rvList) RecyclerView rvList;
-    @BindView(R.id.toolbar) Toolbar tb;
+    @BindView(R.id.rvList)
+    RecyclerView rvList;
+    @BindView(R.id.toolbar)
+    Toolbar tb;
+    @BindView(R.id.dwLayout)
+    DrawerLayout dwLayout;
     private RVAdapter adapter;
 
 
@@ -21,13 +27,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_drawer);
         ButterKnife.bind(this);
-
         setSupportActionBar(tb);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dwLayout,
+                tb, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        dwLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         adapter = new RVAdapter(this);
         rvList.setAdapter(adapter);
         rvList.setLayoutManager(new LinearLayoutManager(this));
-        for(int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             adapter.addEntity("ENT");
         }
     }
