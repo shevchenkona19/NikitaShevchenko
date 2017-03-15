6ad75913ee16e8847586fcc2299143ad7d55b1ef
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-03-15 17:11:46 +0200

    Fullfilled NavDrawer

--- app/src/main/java/sheva/cardapptrue/MainActivity.java
+++ app/src/main/java/sheva/cardapptrue/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_drawer);
         ButterKnife.bind(this);
 
         setSupportActionBar(tb);
