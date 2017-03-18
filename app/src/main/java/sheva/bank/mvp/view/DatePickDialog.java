3e13cd368cfc91ddb8c49d2e282dda2b202d9f91
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-03-18 21:55:26 +0200

    Dialog for picking a date

---
+++ app/src/main/java/sheva/bank/mvp/view/DatePickDialog.java
@@ -0,0 +1,64 @@
+package sheva.bank.mvp.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.ButterKnife;
+import sheva.bank.R;
+import sheva.bank.mvp.view.interfaces.IDialogFragmentView;
+
+/**
+ * Created by shevc on 18.03.2017.
+ */
+
+public class DatePickDialog extends DialogFragment implements IDialogFragmentView{
+
+    private static IGetDateFromDialog getDate;
+
+    interface IGetDateFromDialog{
+        void getDateFromDialog(String date);
+    }
+
+    public DatePickDialog(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View v = inflater.inflate(R.layout.pick_date_dialog, container, false);
+        Button btnOk = (Button) v.findViewById(R.id.btnOk);
+        final EditText etDate = (EditText) v.findViewById(R.id.etDate);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDate.getDateFromDialog(etDate.getText().toString());
+                getDialog().dismiss();
+            }
+        });
+
+        return v;
+    }
+
+    public static DatePickDialog newInstance(Context context) {
+
+        Bundle args = new Bundle();
+        getDate = (IGetDateFromDialog) context;
+        DatePickDialog fragment = new DatePickDialog();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void sendRequestForDate(String date) {
+
+    }
+}
