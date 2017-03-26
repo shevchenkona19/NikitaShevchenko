400118150f9d6f88bd1ef8ccacdb11dfa6198769
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-03-26 11:00:58 +0300

    Created activity for news

---
+++ app/src/main/java/sheva/economicprovider/mvp/ui/activities/NewsActivity.java
@@ -0,0 +1,15 @@
+package sheva.economicprovider.mvp.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sheva.economicprovider.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
