27ceed78c630fd66d2637d2ddac1a6fb24cc7944
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-04-07 17:17:19 +0300

    Fragments RecyclerView impl

--- app/src/main/java/sheva/newsprovider/mvp/ui/fragments/FeedFragment.java
+++ app/src/main/java/sheva/newsprovider/mvp/ui/fragments/FeedFragment.java
@@ -1,11 +1,37 @@
 package sheva.newsprovider.mvp.ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.arellomobile.mvp.MvpAppCompatFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import sheva.newsprovider.R;
 
 /**
  * Created by shevc on 06.04.2017.
- *
+ *d
  */
 
 public class FeedFragment extends MvpAppCompatFragment {
+    @BindView(R.id.rvFeedList)
+    RecyclerView rvList;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ButterKnife.bind(this, container);
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
 }
--- app/src/main/java/sheva/newsprovider/mvp/ui/fragments/NewsFragment.java
+++ app/src/main/java/sheva/newsprovider/mvp/ui/fragments/NewsFragment.java
@@ -1,6 +1,17 @@
 package sheva.newsprovider.mvp.ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.arellomobile.mvp.MvpAppCompatFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import sheva.newsprovider.R;
 
 /**
  * Created by shevc on 06.04.2017.
@@ -8,5 +19,13 @@ import com.arellomobile.mvp.MvpAppCompatFragment;
  */
 
 public class NewsFragment extends MvpAppCompatFragment{
-    //todo implement fragment
+    @BindView(R.id.rvNewsList)
+    RecyclerView rvList;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ButterKnife.bind(this, container);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
