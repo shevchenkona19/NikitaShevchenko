1213a3e5ed353ffba0bc85b564e5817a88f276f6
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-04-07 17:09:52 +0300

    design for settings activity

--- app/src/main/java/sheva/newsprovider/mvp/ui/activities/SettingsActivity.java
+++ app/src/main/java/sheva/newsprovider/mvp/ui/activities/SettingsActivity.java
@@ -1,16 +1,31 @@
 package sheva.newsprovider.mvp.ui.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.Switch;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import sheva.newsprovider.R;
 
 public class SettingsActivity extends MvpAppCompatActivity {
+    @BindView(R.id.tbSettingsToolbar)
+    Toolbar tbSettings;
+    @BindView(R.id.swSettingsNotify)
+    Switch swNotify;
+    @BindView(R.id.btnSettingsChangeInt)
+    Button btnChangeInt;
+    @BindView(R.id.btnSettingsExit)
+    Button btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+        setSupportActionBar(tbSettings);
     }
 }
