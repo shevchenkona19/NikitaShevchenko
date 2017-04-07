86312d9ef7c7ac7c6ea460ecf627bd8968bd0085
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-04-07 16:20:03 +0300

    News Item design

--- app/src/main/java/sheva/newsprovider/mvp/ui/activities/NewsItem.java
+++ app/src/main/java/sheva/newsprovider/mvp/ui/activities/NewsItem.java
@@ -1,16 +1,31 @@
 package sheva.newsprovider.mvp.ui.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import sheva.newsprovider.R;
 
 public class NewsItem extends MvpAppCompatActivity {
-//todo implement Item func
+    @BindView(R.id.tbNewsItemToolbar)
+    Toolbar tbToolbar;
+    @BindView(R.id.ivNewsItemHeaderIMG)
+    ImageView ivHeader;
+    @BindView(R.id.tvNewsItemHeadline)
+    TextView tvHeadline;
+    @BindView(R.id.wvNewsItemWeb)
+    WebView wvWeb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_item);
+        ButterKnife.bind(this);
+        setSupportActionBar(tbToolbar);
     }
 }
