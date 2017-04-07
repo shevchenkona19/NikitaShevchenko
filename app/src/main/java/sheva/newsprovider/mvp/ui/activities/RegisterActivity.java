f85408b89288e3f9b63c9a92f27214e6d79f6bd8
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-04-07 17:09:33 +0300

    design for register activity

--- app/src/main/java/sheva/newsprovider/mvp/ui/activities/RegisterActivity.java
+++ app/src/main/java/sheva/newsprovider/mvp/ui/activities/RegisterActivity.java
@@ -1,16 +1,33 @@
 package sheva.newsprovider.mvp.ui.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import sheva.newsprovider.R;
 
 public class RegisterActivity extends MvpAppCompatActivity {
+    @BindView(R.id.tbRegisterToolbar)
+    Toolbar tbRegister;
+    @BindView(R.id.ivRegisterUserIMG)
+    ImageView ivUserIMG;
+    @BindView(R.id.etRegisterName)
+    EditText etName;
+    @BindView(R.id.etRegisterUsername)
+    EditText etUsername;
+    @BindView(R.id.etRegisterPassword)
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        setSupportActionBar(tbRegister);
     }
 }
