cb7fef1b7c510b9235ce2cdbcb29cf37c1513a0f
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-04-07 16:03:02 +0300

    added navigation drawer

--- app/src/main/java/sheva/newsprovider/mvp/ui/activities/MainActivity.java
+++ app/src/main/java/sheva/newsprovider/mvp/ui/activities/MainActivity.java
@@ -1,7 +1,12 @@
 package sheva.newsprovider.mvp.ui.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 
@@ -12,6 +17,10 @@ import sheva.newsprovider.R;
 public class MainActivity extends MvpAppCompatActivity {
     @BindView(R.id.tbMainToolbar)
     Toolbar tbMain;
+    @BindView(R.id.dwMainDrawer)
+    DrawerLayout dwMain;
+    @BindView(R.id.navMainNavigation)
+    NavigationView navMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,19 @@ public class MainActivity extends MvpAppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(tbMain);
+        //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, dwMain, tbMain, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        dwMain.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navMain.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 }
